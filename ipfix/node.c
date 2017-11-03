4ce8c58984ef49b972e279090c4aba01a780fe69
Jessica Tallon <tsyesika@igalia.com>
2017-11-03 20:22:18 +0100

    Try to trace IP address

--- ipfix/node.c
+++ ipfix/node.c
@@ -12,9 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <string.h>
 #include <vlib/vlib.h>
 #include <vnet/ip/format.h>
 #include <vnet/ip/ip4_packet.h>
+#include <vnet/ethernet/ethernet.h>
 #include <vnet/vnet.h>
 #include <vnet/pg/pg.h>
 #include <vppinfra/error.h>
@@ -26,6 +28,7 @@ typedef struct {
   u32 next_index;
   u32 sw_if_index;
   ip4_address_t src;
+  ip4_header_t* header;
 } ipfix_trace_t;
 
 /* packet trace format function */
@@ -34,9 +37,11 @@ static u8 * format_ipfix_trace (u8 * s, va_list * args)
   CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
   CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
   ipfix_trace_t * t = va_arg (*args, ipfix_trace_t *);
-  
-  s = format (s, "IPFIX: sw_if_index %d, next index %d, src %U\n",
-              t->sw_if_index, t->next_index, format_ip4_address, &t->src);
+
+  s = format (s, "IPFIX: sw_if_index %d, next index %d, src %U, IPv4 header %U\n",
+              t->sw_if_index, t->next_index,
+	      format_ip4_address, &t->src,
+	      format_ip4_header, t->header);
 
   return s;
 }
@@ -73,6 +78,9 @@ ipfix_node_fn (vlib_main_t * vm,
   ipfix_next_t next_index;
   u32 pkts_swapped = 0;
 
+  // Turn debugging on - remove me once fixed.
+  node->flags = VLIB_NODE_FLAG_TRACE;
+
   from = vlib_frame_vector_args (frame);
   n_left_from = frame->n_vectors;
   next_index = node->cached_next_index;
@@ -91,16 +99,17 @@ ipfix_node_fn (vlib_main_t * vm,
           u32 sw_if_index0, sw_if_index1;
           u8 tmp0[6], tmp1[6];
           ethernet_header_t *en0, *en1;
+	  ip4_header_t *ip0, *ip1;
           u32 bi0, bi1;
 	  vlib_buffer_t * b0, * b1;
-          
+
 	  /* Prefetch next iteration. */
 	  {
 	    vlib_buffer_t * p2, * p3;
-            
+
 	    p2 = vlib_get_buffer (vm, from[2]);
 	    p3 = vlib_get_buffer (vm, from[3]);
-            
+
 	    vlib_prefetch_buffer_header (p2, LOAD);
 	    vlib_prefetch_buffer_header (p3, LOAD);
 
@@ -121,9 +130,12 @@ ipfix_node_fn (vlib_main_t * vm,
 
           ASSERT (b0->current_data == 0);
           ASSERT (b1->current_data == 0);
-          
+
           en0 = vlib_buffer_get_current (b0);
           en1 = vlib_buffer_get_current (b1);
+
+	  ip0 = (ip4_header_t *)((u8*)b0->data + sizeof(ethernet_header_t));
+	  ip1 = (ip4_header_t *)((u8*)b1->data + sizeof(ethernet_header_t));
 
           sw_if_index0 = vnet_buffer(b0)->sw_if_index[VLIB_RX];
           sw_if_index1 = vnet_buffer(b1)->sw_if_index[VLIB_RX];
@@ -137,24 +149,26 @@ ipfix_node_fn (vlib_main_t * vm,
 
           if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)))
             {
-              if (b0->flags & VLIB_BUFFER_IS_TRACED) 
+              if (b0->flags & VLIB_BUFFER_IS_TRACED)
                 {
-                    ipfix_trace_t *t = 
+                    ipfix_trace_t *t =
                       vlib_add_trace (vm, node, b0, sizeof (*t));
                     t->sw_if_index = sw_if_index0;
                     t->next_index = next0;
-                    t->src = ((ip4_header_t*)en0)->src_address;
+                    t->src = ip0->src_address;
+		    t->header = ip0;
                   }
-                if (b1->flags & VLIB_BUFFER_IS_TRACED) 
+                if (b1->flags & VLIB_BUFFER_IS_TRACED)
                   {
-                    ipfix_trace_t *t = 
+                    ipfix_trace_t *t =
                       vlib_add_trace (vm, node, b1, sizeof (*t));
                     t->sw_if_index = sw_if_index1;
                     t->next_index = next1;
-                    t->src = ((ip4_header_t*)en1)->src_address;
+                    t->src = ip1->src_address;
+		    t->header = ip1;
                   }
               }
-            
+
             /* verify speculative enqueues, maybe switch current next frame */
             vlib_validate_buffer_enqueue_x2 (vm, node, next_index,
                                              to_next, n_left_to_next,
@@ -169,6 +183,7 @@ ipfix_node_fn (vlib_main_t * vm,
           u32 sw_if_index0;
           u8 tmp0[6];
           ethernet_header_t *en0;
+	  ip4_header_t *ip0;
 
           /* speculatively enqueue b0 to the current next frame */
 	  bi0 = from[0];
@@ -179,28 +194,30 @@ ipfix_node_fn (vlib_main_t * vm,
 	  n_left_to_next -= 1;
 
 	  b0 = vlib_get_buffer (vm, bi0);
-          /* 
+          /*
            * Direct from the driver, we should be at offset 0
            * aka at &b0->data[0]
            */
           ASSERT (b0->current_data == 0);
-          
+
           en0 = vlib_buffer_get_current (b0);
+	  ip0 = (ip4_header_t *)((u8*)b0->data + sizeof(ethernet_header_t));
 
           sw_if_index0 = vnet_buffer(b0)->sw_if_index[VLIB_RX];
 
           /* Send pkt back out the RX interface */
           vnet_buffer(b0)->sw_if_index[VLIB_TX] = sw_if_index0;
 
-          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE) 
+          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)
                             && (b0->flags & VLIB_BUFFER_IS_TRACED))) {
-            ipfix_trace_t *t = 
+            ipfix_trace_t *t =
                vlib_add_trace (vm, node, b0, sizeof (*t));
             t->sw_if_index = sw_if_index0;
             t->next_index = next0;
-            t->src = ((ip4_header_t*)en0)->src_address;
-            }
-            
+            t->src = ip0->src_address;
+	    t->header = ip0;
+	  }
+
           pkts_swapped += 1;
           (&ipfix_main)->packet_counter += 1;
 
@@ -213,7 +230,7 @@ ipfix_node_fn (vlib_main_t * vm,
       vlib_put_next_frame (vm, node, next_index, n_left_to_next);
     }
 
-  vlib_node_increment_counter (vm, ipfix_node.index, 
+  vlib_node_increment_counter (vm, ipfix_node.index,
                                IPFIX_ERROR_SWAPPED, pkts_swapped);
   return frame->n_vectors;
 }
@@ -224,7 +241,7 @@ VLIB_REGISTER_NODE (ipfix_node) = {
   .vector_size = sizeof (u32),
   .format_trace = format_ipfix_trace,
   .type = VLIB_NODE_TYPE_INTERNAL,
-  
+
   .n_errors = ARRAY_LEN(ipfix_error_strings),
   .error_strings = ipfix_error_strings,
 
