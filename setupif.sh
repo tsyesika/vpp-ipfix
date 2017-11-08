e7f61edfc90c3360cd03b598158051dae54103be
Jessica Tallon <tsyesika@igalia.com>
2017-11-08 17:06:12 +0100

    Add setup scripts for VPP and Shell

---
+++ setupif.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case "$1" in
+     "create")
+         ip link add name vpp1out type veth peer name vpp1host
+         ip link set dev vpp1out up
+         ip link set dev vpp1host up
+         ip addr add 10.10.1.1/24 dev vpp1host;;
+     "remove")
+         ip link set dev vpp1out down
+         ip link set dev vpp1host down
+         ip del vpp1out;;
+     "ping")
+         ping -c 3 10.10.1.2;;
+     *)
+         echo "Usage $0 create|remove|ping"
+         exit 1;;
+esac
